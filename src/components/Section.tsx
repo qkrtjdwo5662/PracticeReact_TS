3756d2b159acd9f79d92675fe6d7a33e8dbf1e33
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-05-12 22:13:08 +0900

    Update Section.tsx

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -8,6 +8,9 @@ export default function Section() {
       Lorem ipsum, dolor sit amet consectetur adipisicing elit.
       Ipsa fuga laboriosam et vitae labore nam a id exercitationem,
       eius iste culpa quis pariatur, inventore eligendi animi porro aliquam odit impedit.  
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Ipsa fuga laboriosam et vitae labore nam a id exercitationem,
+      eius iste culpa quis pariatur, inventore eligendi animi porro aliquam odit impedit.  
       </p>
       <button className={styles.btn}>버튼</button>
     </div>
